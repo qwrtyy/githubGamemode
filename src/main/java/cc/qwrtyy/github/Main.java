24a7ff30f71e220e66c7187e0fbaf6c35be3aff7
qwrtyy <topanaprzypale@gmail.com>
2022-12-02 14:01:30 +0100

    async v3

--- src/main/java/cc/qwrtyy/github/Main.java
+++ src/main/java/cc/qwrtyy/github/Main.java
@@ -4,9 +4,10 @@ import cc.qwrtyy.github.commands.GamemodeCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
+
+    private final long load = System.currentTimeMillis() - System.currentTimeMillis(); //async
     @Override
     public void onEnable() {
-        final long load = System.currentTimeMillis() - System.currentTimeMillis(); //async
         Bukkit.getLogger().info("");
         Bukkit.getLogger().info("### Trwa wczytywanie pluginu na gamemode ###");
         getCommand("gamemode").setExecutor(new GamemodeCommand());
